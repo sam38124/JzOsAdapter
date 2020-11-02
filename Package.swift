fc7e7a87a80efb5123a379873bb728ed7c865e60
sam38124 <sam38124@gmail.com>
2020-11-02 17:16:10 +0800

    1.0.1

--- Package.swift
+++ Package.swift
@@ -23,9 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JzOsAdapter",
-            exclude: ["basecell.xib"],
+            exclude: ["CoverCell.xib"],
             resources: [
-              .process("basecell.xib")
+              .process("CoverCell.xib")
             ]),
         .testTarget(
             name: "JzOsAdapterTests",
